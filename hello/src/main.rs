99c10a849fd304dba044686fbdff2ce94234c2e0
Jean-Paul Weijers <18416852+jpweijers@users.noreply.github.com>
2022-11-21 12:59:39 +0000

    3 langs

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+    let dutch = "Hallo, wereld!";
+    let french = "Bonjour!";
+    let languages = [dutch, french];
+    for language in languages.iter() {
+        println!("{}", language);
+    }
 }
